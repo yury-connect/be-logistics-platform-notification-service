db074381ed164440257394fbda77e91b9a7984a7
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-05-15 18:26:35 +0300

    init commit // еще не дапускал, первый комит

---
+++ src/main/java/com/innowise/logistics/notificationservice/BeLogisticsPlatformNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.logistics.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeLogisticsPlatformNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeLogisticsPlatformNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innowise/logistics/notificationservice/BeLogisticsPlatformNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.logistics.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeLogisticsPlatformNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
