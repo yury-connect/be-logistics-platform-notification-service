625d25a7d6eb95a82200f69fe0164a11c7f75840
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-05-15 19:53:59 +0300

    Репозиторий для MongoDB

--- src/main/java/com/innowise/logistics/notificationservice/repository/NotificationRepository.java
+++ src/main/java/com/innowise/logistics/notificationservice/repository/NotificationRepository.java
@@ -1,11 +1,11 @@
 package com.innowise.logistics.notificationservice.repository;
 
 import com.innowise.logistics.notificationservice.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository extends MongoRepository<Notification, String> {
 
     boolean existsByKafkaEventId(String kafkaEventId);
 }
